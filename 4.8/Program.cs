45113b8617bec561dfc01fae882965a7f609bd6a
danil <danil@DESKTOP-KLFI3LI>
2022-09-28 14:58:33 +0200

    Добавьте файлы проекта.

---
+++ 4.8/Program.cs
@@ -0,0 +1,9 @@
+﻿//Голубихин Данила 22-ИСП-2/2
+Console.WriteLine("Введите расстрояние в километрах:");
+double kms = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите расстрояние в футах:");
+double fs = double.Parse(Console.ReadLine());
+double kms1 = kms * 1000;
+double fs1 = fs / 0.305;
+if (kms1 < fs1) Console.WriteLine("Расстояние в километрах меньше");
+else Console.WriteLine("Расстояние в футах меньше");
